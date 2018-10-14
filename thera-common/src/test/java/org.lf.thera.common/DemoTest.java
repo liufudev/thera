e29450e501a6e65c404b9182246224aa4b0a6d69
liufu <liufu>
2018-10-14 23:02:09 +0800

    Initial commit

---
+++ thera-common/src/test/java/org.lf.thera.common/DemoTest.java
@@ -0,0 +1,7 @@
+package org.lf.thera.common;
+
+/**
+ * @author liufu
+ */
+public class DemoTest {
+}
---
+++ thera-test/src/main/java/org/lf/thera/test/PrcFramework.java
@@ -0,0 +1,10 @@
+package org.lf.thera.test;
+
+/**
+ * @author liufu
+ */
+
+public class PrcFramework {
+    public static void main(String[] args) {
+    }
+}
