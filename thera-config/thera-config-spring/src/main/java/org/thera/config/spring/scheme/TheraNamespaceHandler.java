cb5d0b15f78fbb50b7dd664f03e9692c461595e3
liufudev <liufu_dev@qq.com>
2018-11-06 19:49:35 +0800

    add

---
+++ thera-config/thera-config-spring/src/main/java/org/thera/config/spring/scheme/TheraNamespaceHandler.java
@@ -0,0 +1,13 @@
+package org.thera.config.spring.scheme;
+
+import org.springframework.beans.factory.xml.NamespaceHandlerSupport;
+
+/**
+ * @author liufu
+ */
+public class TheraNamespaceHandler extends NamespaceHandlerSupport {
+
+    public void init() {
+        
+    }
+}
